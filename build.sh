0e1687b774770da752910f758b2e68eeafd75d14
nilocj <nilocj@outlook.com>
2024-04-11 11:48:45 -0300

    new build scriptx

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-#
-# Build of TauDEM for user install in ~/.local prefix
-#
-rm -rf src/build
-mkdir -p src/build
-cd src/build && cmake -DCMAKE_INSTALL_PREFIX=~/.local ..
-make test
-make install
