1759d1b45dd1ef33a8c550f0bfa140221c7c1783
nilocj <nilocj@outlook.com>
2025-11-06 11:35:07 -0300

    Update src/CMakeLists.txt

--- setvars.bat
+++ setvars.bat
@@ -1,14 +1,12 @@
 @set VSINSTALLDIR="C:\Program Files\Microsoft Visual Studio\2022\Community"
-REM @set "VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2022"
-@set MSMPI
-REM @call "%VSINSTALLDIR%\Launch-VsDevShell.ps1"
 
-@set "GDAL_DIR=%CONDA_PREFIX%\pixi\envs\windows\Library"
+@set MSMPI
+@set "GDAL_DIR=.pixi\envs\windows\Library"
 @set "GDAL_INCLUDE_DIR=%GDAL_DIR%\include"
 @set "GDAL_LIBRARY_PATH=%GDAL_DIR%\bin\gdal.dll"
 @set "GEOS_LIBRARY_PATH=%GDAL_DIR%\bin\geos_c.dll"
 @set "LIBDIR=%GDAL_DIR%;%GDAL_DIR%\lib"
 @set "GDAL_LIBRARY=%GDAL_LIBRARY_PATH%"
-SET CMAKE_BUILD_TYPE="Release"
 @set "INCLUDE=%INCLUDE%;%GDAL_INCLUDE_DIR%"
 @set "LIB=%LIB%;%LIBDIR%"
+@set CMAKE_BUILD_TYPE="Release"
