ba40ebddfe9de9e4a29c10383f7025922bc060de
Valdenor Nilo de Carvalho Junior <nilocj@outlook.com>
2025-12-03 16:42:54 -0300

    pixi build v0.60

--- setvars.bat
+++ setvars.bat
@@ -1,7 +1,13 @@
 @set VSINSTALLDIR="C:\Program Files\Microsoft Visual Studio\2022\Community"
 
-@set MSMPI
-@set "GDAL_DIR=.pixi\envs\windows\Library"
+REM @set MSMPI 
+REM Resultado do comando set MSMPI nao inclui %CONDA_PREFIX% do ambiente atual
+REM
+@set "MSMPI_BIN=%CONDA_PREFIX%\Library\bin
+@set "MSMPI_INC=%CONDA_PREFIX%\Library\include
+@set "MSMPI_LIB64=%CONDA_PREFIX%\Library\lib
+
+@set "GDAL_DIR=%CONDA_PREFIX%\Library"
 @set "GDAL_INCLUDE_DIR=%GDAL_DIR%\include"
 @set "GDAL_LIBRARY_PATH=%GDAL_DIR%\bin\gdal.dll"
 @set "GEOS_LIBRARY_PATH=%GDAL_DIR%\bin\geos_c.dll"
@@ -9,4 +15,4 @@
 @set "GDAL_LIBRARY=%GDAL_LIBRARY_PATH%"
 @set "INCLUDE=%INCLUDE%;%GDAL_INCLUDE_DIR%"
 @set "LIB=%LIB%;%LIBDIR%"
-@set CMAKE_BUILD_TYPE="Release"
+@set "CMAKE_BUILD_TYPE=Release"
